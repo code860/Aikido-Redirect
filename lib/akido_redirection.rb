4977dd93cb6c3b84d90cff58678beff2786ee602
code860 <benjaminc.barber@gmail.com>
2017-12-03 21:59:26 -0500

    Added some more todos fixed some grammer/typo issues in comments

--- lib/akido_redirection.rb
+++ lib/akido_redirection.rb
@@ -20,13 +20,12 @@ module Aikido
 
     def initialize(app)
       @app = app
-      build_redirect_proc
     end
 
     def call(env)
       request = Rack::Request.new(env)
       if request.get_header(self.configuration.assailant_header)
-        #handle the assailant
+        #Check Header in config and if present run through and redirect the assailant using the following method(s)
         handle_assailant(request, env)
       else
         #Let it through
@@ -37,12 +36,12 @@ module Aikido
 
     private
     def handle_assailant(req, env)
-      proc = build_redirect_proc(load_proc_vars(req))
+      proc = build_redirect_proc(load_proc_vars(req)) # Set up the proc and call it to "handle the assailant and redirect it"
       proc.call(env)
     end
 
     def build_redirect_proc(status_code, url, message)
-      Proc.new{|env| build_redirect_response(status_code, url, message)} #Setup the redirection proc based on the rqeuest type
+      Proc.new{|env| build_redirect_response(status_code, url, message)} #Setup the redirection proc based on the request type
     end
 
     def build_redirect_response(status_code, url, message)
@@ -72,6 +71,7 @@ module Aikido
     end
 
     def build_redirect_url(url, query_options)
+      #TODO- Build a valid URI and convert it back to string
     end
   end
 end
